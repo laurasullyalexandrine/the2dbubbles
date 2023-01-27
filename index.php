56d8d25bf05998d619c7dd9bef6a2569bd81ca82
laurasullyalexandrine <launyie@gmail.com>
2023-01-27 12:48:15 +0100

    update composer

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php 
+<?php
 
-phpinfo();
+require 'vendor/autoload.php';
+
+// Routing
+$page = 'home';
+if (isset($_GET['p'])) {
+    $page = $_GET['p'];
+}
+
+// Render of template
+$loader = new \Twig_loader_Filesystem(__DIR__ . '/templates');
+dd($loader);
+$twig = new \Twig_Environnement($loader, [
+  'cache' => __DIR__ . '/tmp'
+]);
+
+
+if ($page === 'home') {
+    echo $twig->render('home.html.twig');
+}
