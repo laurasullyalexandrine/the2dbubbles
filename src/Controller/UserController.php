204b7f2f018e236a6f6a626479734e6e1473a190
laurasullyalexandrine <launyie@gmail.com>
2023-03-03 16:47:38 +0100

    duplicate functions to read and edit user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,46 @@
 
 namespace app\Controller;
 
+use App\Models\User;
+
 class UserController extends CoreController {
-    
+
+    /**
+     * Listing des user
+     * @return void
+     */
+    public function list()
+    {
+        // On récupère tous les users
+        $userObject = new User();
+        // Récupérer tous les users
+        $users = $userObject->findAll();
+
+        // On les envoie à la vue
+        $this->show(
+            'user/list',
+            [
+                'users' => $users
+            ]
+        );
+    }
+
+    /**
+     * Affiche la vue édition d'un user 
+     *
+     * @param [type] $userId
+     * @return void
+     */
+    public function edit($userId)
+    {
+        $user = User::findBy($userId);
+
+        // On affiche notre vue en transmettant les infos du user
+        $this->show(
+            'user/add-edit',
+            [
+                'user' => $user
+            ]
+        );
+    }
 }
--- src/Models/User.php
+++ src/Models/User.php
@@ -1,12 +1,59 @@
 <?php 
 namespace App\Models;
 
+use PDO;
+use App\Utils\Database;
+
 class User {
     
     private $email;
     private $password;
     private $posts;
-    
+
+    /**
+     * Méthode permettant de récupérer tous les enregistrements de la table user
+     *
+     * @return User
+     */
+    public function findAll() 
+    {
+        // Récupérer de l'objet PDO représentant la connexion à la DB
+        $pdoDBConnexion = Database::getPDO();
+
+        // Ecrire la requête sql
+        $sql = 'SELECT * FROM user';
+
+        // Préparer la requête
+        $pdoStatement = $pdoDBConnexion->prepare($sql);
+
+        // Exécuter la requête
+        $pdoStatement->execute();
+
+        $users = $pdoStatement->fetchAll(PDO::FETCH_CLASS, self::class);
+        
+        return $users;
+    }
+    /**
+     *  Méthode permettant de récupérer un enregistrement de la table User en fonction d'un id donné
+     *
+     * @param [type] $userId
+     * @return User
+     */
+    public static function findBy($userId) 
+    {
+        $pdoDBConnexion = Database::getPDO();
+        $sql = '
+        SELECT * 
+        FROM user 
+        WHERE id = :id';
+        $pdoStatement = $pdoDBConnexion->prepare($sql);
+        $pdoStatement->execute([
+            'id' => $userId
+        ]);
+        $user = $pdoStatement->fetchObject(self::class);
+ 
+        return $user;
+    }
 
     /**
      * Get the value of email
