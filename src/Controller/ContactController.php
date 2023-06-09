350f263e048fc722c24510195b0297f0aeb58c31
laurasullyalexandrine <launyie@gmail.com>
2023-06-09 15:22:16 +0200

    continue to fix the errors codacy about indentation into ContactController file

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,40 +8,41 @@ use Exception;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\PHPMailer;
 
-class ContactController extends CoreController {
+class ContactController extends CoreController
+{
 
-    public function mailSend() {
+    public function mailSend()
+    {
+        if ($this->isPost()) {
+            $subject = filter_input(INPUT_POST, 'subject');
+            $pseudo = filter_input(INPUT_POST, 'pseudo');
+            $email = filter_input(INPUT_POST, 'email');
+            $message = filter_input(INPUT_POST, 'message');
 
-            if ($this->isPost()) {
-                $subject = filter_input(INPUT_POST, 'subject');
-                $pseudo = filter_input(INPUT_POST, 'pseudo');
-                $email = filter_input(INPUT_POST, 'email');
-                $message = filter_input(INPUT_POST, 'message');
-                
-                try {
-                    if (empty($subject)) {
-                        throw new \Exception("Ah mais là on n'sait pas de quoi tu parles...");
-                    }
-                    
-                    if (empty($pseudo)) {
-                       throw new \Exception("Ton pseudo aussi ?");
-                    }
-                    
-                    if (empty($email)) {
-                       throw new \Exception("Ton petit mail pour traiter au mieux ton Bubbles message.");
-                    }
-    
-                    if (empty($message)) {
-                       throw new \Exception("Ah ?! Mais tu n'as rien à nous dire. Quel dommage...");
-                    }
-    
-                    $this->messageSend($subject, $pseudo, $email, $message, [
-                        'to', 
-                    ]);
-                } catch (\Exception $e) {
-                    $this->flashes('warning', $e->getMessage());
+            try {
+                if (empty($subject)) {
+                    throw new \Exception("Ah mais là on n'sait pas de quoi tu parles...");
                 }
+
+                if (empty($pseudo)) {
+                    throw new \Exception("Ton pseudo aussi ?");
+                }
+
+                if (empty($email)) {
+                    throw new \Exception("Ton petit mail pour traiter au mieux ton Bubbles message.");
+                }
+
+                if (empty($message)) {
+                    throw new \Exception("Ah ?! Mais tu n'as rien à nous dire. Quel dommage...");
+                }
+
+                $this->messageSend($subject, $pseudo, $email, $message, [
+                    'to',
+                ]);
+            } catch (\Exception $e) {
+                $this->flashes('warning', $e->getMessage());
             }
+        }
         $this->show('/front/email/contact');
     }
 }
